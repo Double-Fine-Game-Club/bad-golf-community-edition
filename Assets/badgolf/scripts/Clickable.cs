9c75018ae8f21633ab3b105d8778ad0f625de33f
Erik Behar <ebehar@thirdmotion.com>
2014-02-11 19:44:32 -0800

    added some sorta order

---
+++ Assets/badgolf/scripts/Clickable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour 
+{
+	public Camera theCam;
+	public GameObject target;
+	public string messageName;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Input.GetMouseButtonUp(0) )
+		{
+			Ray ray = theCam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				if ( hit.collider.gameObject == this.gameObject )
+					target.SendMessage(messageName);
+			}
+		}
+	}
+}
---
+++ Assets/badgolf/scripts/GameControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour 
+{
+	public GameObject ed_menuScreen;
+
+	private GameObject loadedLevel;
+
+	private bool levelLoaded = false;
+
+	public void onStartClicked()
+	{
+		//TODO: get value from selection thing in menu
+		Application.LoadLevelAdditive( "level_01");
+		ed_menuScreen.SetActive(false);
+	}
+
+	void Update()
+	{
+		if ( levelLoaded )
+		{
+			loadedLevel = transform.root.FindChild( "level_01").gameObject;
+			levelLoaded = false;
+		}
+	}
+}
