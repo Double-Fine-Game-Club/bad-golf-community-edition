7c1af5c820b4abdeb210fc3974dfbfa98e5c9093
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-02-17 00:16:27 +0200

    Very basic ball hitting

---
+++ Assets/badgolf/scripts/SwingMode.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingMode : MonoBehaviour
+{
+	
+		public GameObject camera;
+		private Vector3 cameraPos = new Vector3 (0, 2, -4);
+		private float shotPower = 0f;
+		public int hitMultiplier = 5;
+		private bool flying = false;
+
+	
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+
+		void OnGUI ()
+		{
+				GUI.Box (new Rect (200, 200, 100, 100), "power: " + (int)shotPower);
+		}
+
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Debug.Log ("Flying: " + flying);
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+						rigidbody.freezeRotation = false;
+						gameObject.transform.Rotate (0f, 1f, 0f);
+						rigidbody.freezeRotation = true;
+				}
+				if (Input.GetKey (KeyCode.RightArrow)) {
+						rigidbody.freezeRotation = false;
+						gameObject.transform.Rotate (0f, -1f, 0f);
+						rigidbody.freezeRotation = true;
+				}
+		
+		
+				Vector3 newPos = transform.position + transform.localRotation * cameraPos;
+				float lerper = Mathf.Min ((camera.transform.position - newPos).sqrMagnitude / 100, 1);
+				camera.transform.position = (1 - lerper) * camera.transform.position + lerper * newPos;
+				camera.transform.rotation = Quaternion.Lerp (camera.transform.rotation, Quaternion.LookRotation (transform.position - camera.transform.position), lerper);
+
+				if (flying) {
+						return;
+				}
+
+				if (Input.GetAxis ("Vertical") > 0) {
+						shotPower += Input.GetAxis ("Vertical") * hitMultiplier;
+						if (shotPower > 500) {
+								shotPower = 500;
+						}
+				}
+		
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						Debug.Log ("BOOM: " + shotPower);
+						flying = true;
+						Vector3 arc = Vector3.forward;
+						arc.y = arc.y + shotPower / 200;
+						rigidbody.AddForce (transform.localRotation * arc * shotPower);
+						shotPower = 0;
+				}
+		}
+
+		void OnCollisionEnter (Collision col)
+		{
+				if (col.gameObject.tag == "Ground") {
+						flying = false;
+				}
+		}
+}
