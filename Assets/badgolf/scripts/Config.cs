0e8463bfb2ecd310e6057798edd95afcb7bae0ce
Goldenheaven <theshadow_@hotmail.com>
2014-02-17 01:40:16 +0000

    Level Loading, Config File and Menu Functionallity
    * Changes to clickable script:
    
    Commented Update Section ( To be Uncommented if
    Input.simulateMouseWithTouches doesn't allow touch to call mouse events
    ).
    
    When mouse is over avaible options will turn text color red.
    
    When mouse is no longer over the text will change color back to white.
    
    * Changes to GameControl script:
    
    Changed Application.LoadLevelAdditive to make it load from selected
    menu level.
    
    Commented Update Section as it wasn't doing anything.
    
    * Changed intro object and object that holds current level text to
    allow:
    
    Added Tag "Intro" to intro object
    
    Added Tag "LevelID" to current level object
    
    changed the name of current level object to levelID
    
    * Created script Config to allow load of files:
    
    Write basic config xml as start
    
    Function to load levels
    
    * Created script LevelSelect to allow level selection on menu:
    
    Loads level names from Config.cs
    
    Added to change level select on arrow key input
    
    *Changes to config.cs & gamecontrol.cs:
    
    Allow levels to be loaded on the path specified on the config.xml, but
    it will only work after the second time that it is ran if you change the
    default path. (unity free limitation)

---
+++ Assets/badgolf/scripts/Config.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Xml = System.Xml;
+using ConfigWriter = System.Xml.XmlWriter;
+using ConfigReader = System.Xml.XmlDocument;
+
+
+public class Config : MonoBehaviour {
+	
+	private string configPath = "Assets\\badgolf\\config.xml";
+	
+	// Use this for initialization
+	void Start () {
+		if (System.IO.File.Exists (configPath) == false){
+			Xml.XmlWriterSettings settings = new Xml.XmlWriterSettings();
+			settings.Indent = true;
+			
+			string levelsPath = "Assets//badgolf//scenes";
+			string[] loaderPath = System.IO.Directory.GetFiles (levelsPath);
+			ConfigWriter cfgFile = ConfigWriter.Create(configPath,settings);
+			cfgFile.WriteStartDocument();
+			cfgFile.WriteStartElement("Config");
+			cfgFile.WriteStartElement("Levels");
+			cfgFile.WriteAttributeString ("LevelsPath",levelsPath.ToString());
+			
+			foreach (string file in loaderPath){
+				
+				if (file.EndsWith(".meta") == false)
+				{
+					cfgFile.WriteElementString("Map",file.Remove(0,levelsPath.Length+1).Remove(file.Length-(levelsPath.Length+7)).ToString());
+				}
+				
+			}
+			
+			cfgFile.WriteEndElement();
+			cfgFile.WriteEndElement ();	
+			cfgFile.WriteEndDocument();
+			cfgFile.Flush ();
+			cfgFile.Close ();
+		}
+	}
+	
+	public void cfgLoadLevels(){
+		ConfigReader cfgFile = new ConfigReader ();
+		cfgFile.Load (configPath);
+		
+		GameObject gObj = GameObject.FindGameObjectWithTag ("LevelID");
+		LevelSelect levelSel = gObj.GetComponent (typeof(LevelSelect)) as LevelSelect;
+		Xml.XmlNodeList node = cfgFile.GetElementsByTagName ("Map");
+		int mapCount = node.Count;
+		
+		levelSel.levels = new string[mapCount];
+		int itn = 0;
+		
+		foreach (Xml.XmlElement nodes in node){
+			levelSel.levels[itn] = nodes.InnerText;
+			itn = itn+1;
+		}
+	}
+	
+}
