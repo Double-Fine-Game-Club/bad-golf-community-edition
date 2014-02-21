d27dda982efb54908ec7fafea6b07e86bb35a689
Brian Condon <bc5389@gmail.com>
2014-02-21 18:45:40 -0500

    Mini Map Prototype (Deleted Unneccessary files)
    Recommitted mini map prototype deleted build files
    
    
    Former-commit-id: 4acc5332ec871891f561c016e58f10e22d6eb9ed

---
+++ assets/art/shaders/TestShader.shader
@@ -0,0 +1,27 @@
+﻿Shader "Custom/TestShader" {
+	Properties {
+		_ColorTint("Color Tint", Color) = (1, 1, 1, 1)
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		
+		CGPROGRAM
+		#pragma surface surf Lambert
+
+		struct Input {
+			float4 color: Color;
+			float2 uv_MainTex;
+		};
+
+		float4 _ColorTint;
+		sampler2D _MainTex;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			IN.color = _ColorTint;
+			o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb * IN.color;
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
---
+++ assets/scripts/TransferToSwing.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// New script separated from Movement. This only makes sure you can move to Swing Mode when you are close enough and press 'E'.
+
+public class TransferToSwing : MonoBehaviour {
+
+	public GameObject ball;
+	// For updating the GUI Box.
+	private bool inHittingRange = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float distance = Vector3.Distance (gameObject.transform.position, ball.transform.position);
+		if (distance < 5) {
+			inHittingRange = true;
+		} else {
+			inHittingRange = false;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			if (inHittingRange) {
+				ball.SendMessage ("turnOnScripts");
+
+				this.gameObject.SendMessage("turnOffScripts");
+			}
+		}
+	}
+
+	// Makes the ugly GUI box that tells when you are close enough to the ball.
+	void OnGUI ()
+	{
+		GUI.Box (new Rect (200, 200, 100, 100), "in range: " + inHittingRange);
+	}
+}
