7c53dc2e0f626b9873e1da5f16da50f4d1208b4b
ebehar <ebehar@srinteractive.com>
2014-02-22 15:23:27 -0800

    update original menu, to keep pace with the times

---
+++ assets/scripts/gui/Clickable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour
+{
+	public GameObject messageTarget;
+	public string messageName;
+	
+	void OnMouseDown()
+	{
+		messageTarget.SendMessage(messageName);
+	}
+}
---
+++ assets/scripts/gui/HoverableTexture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverableTexture : MonoBehaviour 
+{
+	public GameObject normal;
+	public GameObject hover;
+	
+	void OnEnable()
+	{
+		normal.SetActive(true);
+		hover.SetActive(false);
+	}
+
+	void OnMouseOver()
+	{
+		normal.SetActive(false);
+		hover.SetActive(true);
+	}
+	
+	void OnMouseExit()
+	{
+		normal.SetActive(true);
+		hover.SetActive(false);
+	}
+}
