32d107e48143ce1909f5dbe989a2ed564f7d49c5
thegsm <thegsm@users.noreply.github.com>
2014-02-22 20:30:50 +0200

    Fixed layering (no need to copy the entire map) Changed map following to explicit plane (solves a problem when cart turns over, the cart position disappars)
    Former-commit-id: e31856588525c8e3307338e5e19609c2f92b8324

---
+++ assets/scripts/FollowPositionScript.cs
@@ -0,0 +1,59 @@
+﻿// This camera positions the camera with the same position as the object for some of the XYZ plane components.
+
+using UnityEngine;
+using System.Collections;
+
+public class FollowPositionScript : MonoBehaviour {
+	
+	// The target we are following
+	public Transform target;
+
+	public enum PlaneType {XY, XZ, YZ}
+
+	// Which plane to follow the position
+	public PlaneType followPlane = PlaneType.XZ;
+
+	public float rotationDamping = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void LateUpdate () {
+		// Early out if we don't have a target
+		if (target == null) {
+			return;
+		}
+
+		Vector3 position = gameObject.transform.position;
+		Quaternion rotation = gameObject.transform.rotation;
+		Vector3 rotationEulerAngles = rotation.eulerAngles;
+		
+		if (followPlane == PlaneType.XZ) {
+			position.x = target.position.x;
+			position.z = target.position.z;
+			rotationEulerAngles.y = Mathf.LerpAngle(rotationEulerAngles.y, target.eulerAngles.y, rotationDamping * Time.deltaTime);
+		}
+		else if (followPlane == PlaneType.XY) {
+			position.x = target.position.x;
+			position.z = target.position.y;
+			rotationEulerAngles.z = Mathf.LerpAngle(rotationEulerAngles.z, target.eulerAngles.z, rotationDamping * Time.deltaTime);
+		}
+		else if (followPlane == PlaneType.YZ) {
+			position.x = target.position.y;
+			position.z = target.position.z;
+			rotationEulerAngles.x = Mathf.LerpAngle(rotationEulerAngles.x, target.eulerAngles.x, rotationDamping * Time.deltaTime);
+		}
+		
+		rotation.eulerAngles = rotationEulerAngles;
+
+		gameObject.transform.position = position;
+		gameObject.transform.rotation = rotation;
+
+	}
+}
