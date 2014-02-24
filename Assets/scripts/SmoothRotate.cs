ccf514fe957e8df6194b6995a0ac29448b9871ca
Ronny Reinhardtsen <ronny@ronnrein.com>
2014-02-24 23:20:00 +0100

    Added simple rotation script to work with Smooth Follow

--- Assets/scripts/SmoothRotate.cs
+++ Assets/scripts/SmoothRotate.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class SmoothRotate : MonoBehaviour {
 
-    //Target to rotate around
+    // Target to rotate around
     public Transform target;
 
-    //
+    // Multiplier for mousemovement
     public float mouseMultiplier;
 
-
-
     void LateUpdate() {
+
+        // Check if mouse has moved
         if (Input.GetAxis("Mouse X") != 0) {
             transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * mouseMultiplier * Time.deltaTime);
         }
