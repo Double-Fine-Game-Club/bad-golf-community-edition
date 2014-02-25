fdd4ba8f4787f1268973508de3d9449da6c058eb
Christian Afonso <chr.afonso@gmail.com>
2014-02-25 18:29:22 +0100

    Added animation testing script LookBack. Added function SetFaceExpression() to FaceState.as

---
+++ Assets/scripts/animation/LookBack.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Testing
+public class LookBack : MonoBehaviour {
+	
+	// blend shape indices
+	float goalTurn = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Manual turn
+		if (Input.GetKeyDown (KeyCode.H)) {
+			goalTurn = 180;
+		} else if (Input.GetKeyUp (KeyCode.H)) {
+			goalTurn = 0;
+		}
+
+		float diffTurn = transform.localRotation.eulerAngles.y - goalTurn;
+		if(Mathf.Abs(diffTurn) > 1)
+		{
+			transform.Rotate(new Vector3(0, -Mathf.Sign(diffTurn)*20, 0));
+		}
+	}
+}
