04eceabad1eed7a2e25f53702b7441d0e9e0ceb1
Brian Condon <bc5389@gmail.com>
2014-02-26 17:23:12 -0500

    Player Respawn Script
    Added a script so the player respawns to their start position should
    they fly off the map and below a certain threshold on the y axis

---
+++ Assets/scripts/PlayerRespawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRespawn : MonoBehaviour {
+
+	public int respawnThreshold = 0;
+	private Vector3 playerSpawn;
+	private Quaternion spawnRotation;
+	// Use this for initialization
+	void Start () {
+	//set spawn transform
+	playerSpawn = transform.position;
+	spawnRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < respawnThreshold){
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.angularVelocity = Vector3.zero;
+			transform.rotation = spawnRotation;
+			transform.position = playerSpawn;
+		}
+	}
+}
