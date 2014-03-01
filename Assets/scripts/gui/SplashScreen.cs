fffb63322619ed90bac3daf1ff643df12591d594
Kjell Iwarson <lightsoda@gmail.com>
2014-03-01 14:47:34 +0100

    Basic splash screen selector script. Some changes to offline lobby

---
+++ Assets/scripts/gui/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	public Sprite[] splashScreens;
+
+	// Use this for initialization
+	void Start () {
+		int i = Random.Range(0, splashScreens.Length);
+		SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+		renderer.sprite = splashScreens[i];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
