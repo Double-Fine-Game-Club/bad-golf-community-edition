1b4f8b753a5ee93c958226fababdadd2bade25d3
Cheeseness <contact@jbushproductions.com>
2014-03-04 22:27:36 +1000

    Added F12 screenshot functionality.

---
+++ Assets/scripts/SaveScreenshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveScreenshot : MonoBehaviour
+{
+	private int count = 0;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown("f12")) //TODO: Get this binding out to somewhere useful
+		{
+			string fname;
+			do
+			{
+				count ++;
+				fname = "screenshot_" + count.ToString("D5") + ".png";
+
+			} while (System.IO.File.Exists(fname));
+
+			Application.CaptureScreenshot(Application.persistentDataPath + System.IO.Path.DirectorySeparatorChar + fname);
+		}
+	}
+}
