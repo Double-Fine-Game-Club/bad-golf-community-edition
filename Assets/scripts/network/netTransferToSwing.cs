faa31a10e1148d056abcd603be0bb09f9948243a
osse101 <mikemck101@gmail.com>
2014-04-06 20:45:00 -0230

    ball texture update and swingIcon
    Added ambient light (from offline level_full) to level_full
    Changed ball prefab texture
    Added 2d club image to indicate when ready to swing
    Removed net_swing's gui textbox and replaced it with a ball glow and
    swing_icon

--- Assets/scripts/network/netTransferToSwing.cs
+++ Assets/scripts/network/netTransferToSwing.cs
@@ -6,28 +6,30 @@ using System.Collections;
 public class netTransferToSwing : MonoBehaviour {
 
 	public GameObject ball, cart;
-	// For updating the GUI Box.
+	private Behaviour ballGlow;
+	private GameObject swingIcon;
 	private bool inHittingRange = false;
-	public Rect guiBoxPosition = new Rect (100, 200, 100, 100);
-	public bool showGUI = false;
 	networkVariables nvs;
 	PlayerInfo myInfo;
-	
-	//private Color originalColor = Color.grey;
 
 	void Start()
 	{
-		//originalColor = ball.renderer.material.GetColor("_ColorTint");
 		nvs = GetComponent ("networkVariables") as networkVariables;
 		myInfo = nvs.myInfo;
 		ball = nvs.myInfo.ballGameObject;
 		cart = nvs.myInfo.cartGameObject;
+
+		//Create swing Icon for player
+		swingIcon = Instantiate (Resources.Load ("swing_icon")) as GameObject;
+		swingIcon.transform.parent = nvs.myCam.transform;
+		swingIcon.transform.localPosition = new Vector3 (0f, -0.37f, 1.22f);	
+		swingIcon.SetActive (false);	//starts hidden
+
+		//Get glow effect on ball
+		ballGlow = ball.GetComponent ("Halo") as Behaviour;
+		ballGlow.enabled = false;
 	}	
 
-	void OnEnable(){
-		showGUI = true;
-	}
-	
 	void Update ()
 	{
 
@@ -35,19 +37,18 @@ public class netTransferToSwing : MonoBehaviour {
 		if (distance < 5) 
 		{
 			inHittingRange = true;
-			//Material temp =	ball.renderer.sharedMaterial; 
-			//temp.SetColor("_ColorTint", Color.red);
-			//ball.renderer.sharedMaterial = temp;
+			ballGlow.enabled=true;
+			swingIcon.SetActive (true);
 		} 
 		else 
 		{
 			inHittingRange = false;
-			//Material temp =	ball.renderer.sharedMaterial; 
-			//temp.SetColor("_ColorTint", originalColor);
-			//ball.renderer.sharedMaterial = temp;
+			ballGlow.enabled=false;
+			swingIcon.SetActive (false);
 		}
 		if (!myInfo.playerIsPaused && inHittingRange) 
 		{
+			swingIcon.transform.rotation = nvs.myCam.transform.rotation;	//always facing camera
 			bool attemptGolfActionPressed = Input.GetKeyUp (KeyCode.E);
 			#if !UNITY_EDITOR && (UNITY_IPHONE || UNITY_ANDROID)
 				if (Input.touchCount > 0)
@@ -65,31 +66,32 @@ public class netTransferToSwing : MonoBehaviour {
 
 			if ( attemptGolfActionPressed )
 			{ 
-					//But character at the golf ball
-					gameObject.SendMessage("switchToBall");
-					showGUI=false;
-					this.enabled=false;
+				//But character at the golf ball
+				gameObject.SendMessage("switchToBall");
+				ballGlow.enabled=false;
+				swingIcon.SetActive (false);
+				this.enabled=false;
 			}
 		}
-	}
-	
-	// Makes the ugly GUI box that tells when you are close enough to the ball.
-	void OnGUI ()
-	{
-		if( showGUI)
-			GUI.Box (guiBoxPosition, "in range: " + inHittingRange);
 	}
 	
 	void onUserGamePadButton()
 	{
 		if (inHittingRange) 
 		{
-			if (inHittingRange) 
-			{
-				gameObject.SendMessage("switchToBall");
-				showGUI=false;
-				this.enabled=false;
-			}
+			gameObject.SendMessage("switchToBall");
+			ballGlow.enabled=false;
+			swingIcon.SetActive (false);
+			this.enabled=false;
 		}
+	}
+
+	void OnDisable(){
+		//ballGlow.enabled = false;
+		//swingIcon.SetActive (false);
+	}
+
+	void OnDestroy(){
+		Destroy (swingIcon);
 	}
 }
